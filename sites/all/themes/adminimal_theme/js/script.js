4b27c23e2fab40dfa76f39f037f49858a638d4cf
quangnc <chiquang.dev@gmail.com>
2019-07-29 09:40:00 +0700

    first commit

---
+++ sites/all/themes/adminimal_theme/js/script.js
@@ -0,0 +1,7 @@
+
+function onClickAdd() {
+    const show = document.getElementById('pham-xem-them');
+    const el = document.getElementsByClassName('xem-them')[0];
+    show.style.display = "block";
+    el.innerHTML = "Thu hồi >>";
+}
